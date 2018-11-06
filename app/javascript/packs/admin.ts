a32fc2c1153d39d2886cf638bcd9300559a7b202
Sorah Fukumori <sorah@cookpad.com>
2018-11-07 07:37:27 +0900

    packs/admin.ts

--- app/javascript/packs/admin.ts
+++ app/javascript/packs/admin.ts
@@ -1,10 +1,9 @@
 import 'bootstrap/dist/css/bootstrap';
 import 'font-awesome/css/font-awesome';
 
-import jQuery from 'jquery';
-window.jQuery = jQuery;
-
 import 'bootstrap';
 
 import Rails from 'rails-ujs';
 Rails.start();
+
+import 'user_sponsorships_form';
