7c26ed3386a864e160ca20f66ed5ee3640fbebbc
Sorah Fukumori <sorah@cookpad.com>
2018-11-08 15:49:36 +0900

    Locale switcher

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    if params[:hl] 
+      if I18n.available_locales.include?(params[:hl].to_sym)
+        session[:hl] = params[:hl].to_sym
+      else
+        session.delete(:hl)
+      end
+    end
+    if session[:hl]
+      I18n.locale = session[:hl]
+    end
+  end
+
   helper_method def current_sponsorship
     return @current_sponsorship if defined? @current_sponsorship
     @current_sponsorship = session[:sponsorship_id] && Sponsorship.find_by(id: session[:sponsorship_id])
