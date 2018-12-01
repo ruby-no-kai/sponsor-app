94f4409713c40d962f0860069b9f5054c59ff826
Akira Matsuda <ronnie@dio.jp>
2018-12-01 14:19:00 +0900

    Redirect the users to the default conference if determinable

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,5 +1,9 @@
 class ConferencesController < ApplicationController
   def index
     @conferences = Conference.application_open.order(id: :asc)
+
+    if @conferences.one?
+      redirect_to user_conference_sponsorship_path(@conferences.first)
+    end
   end
 end
