ebebbfa1d08cbb21bb07dbe7f22b9b856d488647
Sorah Fukumori <sorah@cookpad.com>
2019-01-21 03:21:33 +0900

    process_sponsorship_edit_job: kwarg's hard

--- app/jobs/process_sponsorship_edit_job.rb
+++ app/jobs/process_sponsorship_edit_job.rb
@@ -5,7 +5,9 @@ class ProcessSponsorshipEditJob < ApplicationJob
 
     actor = edit.staff ? "#{edit.staff.login} (staff)" : 'sponsor'
     SlackWebhookJob.perform_now(
-      text: ":pencil: <#{conference_sponsorship_url(@conference, @sponsorship)}|#{@sponsorship.name}> edited by #{actor} [#{edit.diff_summary.map{ |_| "`#{_}`" }.join(', ')}] <#{conference_sponsorship_editing_histories_url(@conference, @sponsorship)}|diff>",
+      {
+        text: ":pencil: <#{conference_sponsorship_url(@conference, @sponsorship)}|#{@sponsorship.name}> edited by #{actor} [#{edit.diff_summary.map{ |_| "`#{_}`" }.join(', ')}] <#{conference_sponsorship_editing_histories_url(@conference, @sponsorship)}|diff>",
+      },
       hook_name: :feed,
     )
   end
