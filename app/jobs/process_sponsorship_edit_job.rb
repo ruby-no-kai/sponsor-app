4b1498e93c92c877481a8b53978d14e954c1ed0a
Sorah Fukumori <sorah@cookpad.com>
2019-01-21 01:28:36 +0900

    ProcessSponsorshipEditJob: show who have edited

--- app/jobs/process_sponsorship_edit_job.rb
+++ app/jobs/process_sponsorship_edit_job.rb
@@ -3,8 +3,9 @@ class ProcessSponsorshipEditJob < ApplicationJob
     @sponsorship = edit.sponsorship
     @conference = @sponsorship.conference
 
+    actor = edit.staff ? "#{edit.staff.login} (staff)" : 'sponsor'
     SlackWebhookJob.perform_now(
-      text: ":pencil: <#{conference_sponsorship_url(@conference, @sponsorship)}|#{@sponsorship.name}> [#{edit.diff_summary.map{ |_| "`#{_}`" }.join(', ')}]",
+      text: ":pencil: <#{conference_sponsorship_url(@conference, @sponsorship)}|#{@sponsorship.name}> edited by #{actor} [#{edit.diff_summary.map{ |_| "`#{_}`" }.join(', ')}] <#{conference_sponsorship_editing_histories_url(@conference, @sponsorship)}|diff>",
       hook_name: :feed,
     )
   end
