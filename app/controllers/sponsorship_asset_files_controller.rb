ruby-no-kai/sponsor-app

commit 80992a86077a12e82a9e7c217f2fb7d3e91110d2

Prevent using different conference

app/controllers/sponsorship_asset_files_controller.rb
@@ -1,7 +1,7 @@
 class SponsorshipAssetFilesController < ApplicationController
   def create
     return render(status: 403, json: {error: 403}) if current_sponsorship&.asset_file
-    conference = Conference.find_by!(slug: params[:conference_slug])
+    conference = current_sponsorship.conference
     return render(status: 403, json: {error: 403}) if !conference&.amendment_open? && !current_staff
 
     asset_file = SponsorshipAssetFile.create!(prefix: "#{conference.id}/", extension: params[:extension])

commit 69b309191e802d670a0f17976c295392e1076073

Fix NoMethodError for new sponsorships when uploading assets
Caused by commit 80992a86077a12e82a9e7c217f2fb7d3e91110d2

app/controllers/sponsorship_asset_files_controller.rb
@@ -1,7 +1,7 @@
 class SponsorshipAssetFilesController < ApplicationController
   def create
     return render(status: 403, json: {error: 403}) if current_sponsorship&.asset_file
-    conference = current_sponsorship.conference
+    conference = current_sponsorship ? current_sponsorship.sponsorship : Conference.find_by!(slug: params[:conference_slug])
     return render(status: 403, json: {error: 403}) if !conference&.amendment_open? && !current_staff
 
     asset_file = SponsorshipAssetFile.create!(prefix: "#{conference.id}/", extension: params[:extension])