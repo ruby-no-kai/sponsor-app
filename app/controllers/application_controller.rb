2fff4a46d2597bea26cebcf286eccd071ce5a19c
Sorah Fukumori <sorah@cookpad.com>
2019-03-01 02:45:00 +0900

    Deny withdrawn sponsorship to be logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,8 @@ class ApplicationController < ActionController::Base
 
   helper_method def current_sponsorship
     return @current_sponsorship if defined? @current_sponsorship
-    @current_sponsorship = session[:sponsorship_id] && Sponsorship.find_by(id: session[:sponsorship_id])
+    # XXX: active
+    @current_sponsorship = session[:sponsorship_id] && Sponsorship.active.find_by(id: session[:sponsorship_id])
   end
 
   def require_staff
