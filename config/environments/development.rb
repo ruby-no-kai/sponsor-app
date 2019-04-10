09c97697ef565d60ef302d92b39e68c4c6989830
Sorah Fukumori <sorah@cookpad.com>
2019-04-10 09:11:19 +0900

    :fire: bye ActiveStorage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,9 +35,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
-
   config.active_job.queue_adapter = :inline
   if ENV['ENABLE_SIDEKIQ']
     config.active_job.queue_name_prefix = "sponsor_app"
