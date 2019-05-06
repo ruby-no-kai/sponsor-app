7974beec6222e9ee96692b1e5d730e214629b8f2
Sorah Fukumori <sorah@cookpad.com>
2019-05-06 14:40:16 +0900

    $DISABLE_SIDEKIQ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  config.active_job.queue_adapter = :sidekiq
+  config.active_job.queue_adapter = ENV.fetch('DISABLE_SIDEKIQ', '0') == '1' ? :inline : :sidekiq
 
   config.action_mailer.perform_caching = false
 
