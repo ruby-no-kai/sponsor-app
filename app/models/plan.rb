853700c4ac9eeb4b5f545e65163baa8867ff53a9
Sorah Fukumori <her@sorah.jp>
2019-12-05 07:25:09 +0900

    see only active sponsorship count for stock

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -14,7 +14,7 @@ class Plan < ApplicationRecord
   end
 
   def sold_out?
-    capacity && sponsorships.count < capacity
+    !capacity || sponsorships.active.count > capacity
   end
 
   def closed?(t = Time.zone.now)
