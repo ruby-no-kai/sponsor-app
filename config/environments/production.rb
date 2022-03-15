48e3d7431407a3efa8a42609939a1812e327caae
Sorah Fukumori <sorah@cookpad.com>
2022-03-15 09:15:55 +0900

    session_store=

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
   config.cache_store = :redis_store, ENV['REDIS_URL'], {}
 
-  config.session_store :redis_store, {
+  config.session_store = :redis_store, {
     servers: [ENV['REDIS_URL']],
     expire_after: 14.days,
     key: "_#{Rails.application.class.parent_name.downcase}_session",
