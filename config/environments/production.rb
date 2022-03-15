884d0101289cb0509140e8f4058c1b47997a0bb3
Sorah Fukumori <sorah@cookpad.com>
2022-03-16 01:14:30 +0900

    session_store kwargs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,9 +72,8 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
   config.cache_store = :redis_cache_store, { url: [ENV['REDIS_URL']], pool_size: 5, pool_timeout: 5 }
-  config.session_store :cache_store, {
-    expire_in: 14.days,
-  }
+  config.session_store :cache_store, expire_in: 14.days
+  
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
